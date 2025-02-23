a239bd4095d720737ec50f6bccaccb28dbcb68b2
rocket-fuel86 <NasaSpace65@gmail.com>
2025-02-23 20:56:03 +0200

    commit

---
+++ HW2/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Task 1
+
+            //Console.WriteLine("It's easy to win forgiveness for being wrong;\r\nbeing right is what gets you into real trouble.\r\nBjarne Stroustrup");
+
+            //// Task 2
+
+            //int num1 = int.Parse(Console.ReadLine()!);
+            //int num2 = int.Parse(Console.ReadLine()!);
+            //int num3 = int.Parse(Console.ReadLine()!);
+            //int num4 = int.Parse(Console.ReadLine()!);
+            //int num5 = int.Parse(Console.ReadLine()!);
+
+            //int sum = num1 + num2 + num3 + num4 + num5;
+
+            //int[] tempArray = { num1, num2, num3, num4, num5 };
+
+            //int min = num1;
+            //int max = num1;
+
+            //for (int i = 0; i < tempArray.Length; i++)
+            //{
+            //    if (tempArray[i] < min)
+            //    {
+            //        min = tempArray[i];
+            //    }
+            //}
+
+            //for (int i = 0; i < tempArray.Length; i++)
+            //{
+            //    if (tempArray[i] > max)
+            //    {
+            //        max = tempArray[i];
+            //    }
+            //}
+
+            //int product = num1 * num2 * num3 * num4 * num5;
+
+            //Console.WriteLine(sum + " " + min + " " + max + " " + product);
+
+
+            //// Task 3
+
+            //char[] value = Console.ReadLine()!.ToCharArray();
+
+            //Array.Reverse(value);
+
+            //Console.WriteLine(value);
+
+
+            //// Task 4
+
+            //int start = int.Parse(Console.ReadLine()!);
+            //int end = int.Parse(Console.ReadLine()!);
+
+            //for (int i = 0, j = 1; i <= end;)
+            //{
+            //    if (i >= start)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+
+            //    int next = i + j;
+            //    i = j;
+            //    j = next;
+            //}
+
+
+            //// Task 5
+
+            //int start = int.Parse(Console.ReadLine()!);
+            //int end = int.Parse(Console.ReadLine()!);
+
+            //for (int i = start; i <= end; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+            //// Task 6
+
+            //int length = int.Parse(Console.ReadLine()!);
+            //char symbol = char.Parse(Console.ReadLine()!);
+            //int direction = int.Parse(Console.ReadLine()!);
+
+            //Console.WriteLine();
+
+            //switch (direction)
+            //{
+            //    case 0:
+            //        for (int i = 0; i < length; i++)
+            //        {
+            //            Console.Write(symbol + " ");
+            //        }
+            //        break;
+            //    case 1:
+            //        for (int i = 0; i < length; i++)
+            //        {
+            //            Console.WriteLine(symbol + " ");
+            //        }
+            //        break;
+            //}
+        }
+    }
+}
